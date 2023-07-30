a367cffbd282cd76474fd26df818a2362cdbc062
bruce-x-wu <wu.bruce.x@gmail.com>
2023-07-29 17:10:32 -0700

    feat: add get most recent commit

--- src/index.ts
+++ src/index.ts
@@ -25,41 +25,38 @@ async function getCommits (octokitClient: Octokit, query: string): Promise<Commi
   })
 }
 
-async function getCommitsAfterDateTime (octokitClient: Octokit, query: string, dateTime: string): Promise<Commit[]> {
-  const fullQuery: string = `${query} author-date:>${dateTime}`
-  const commits: Commit[] = []
+async function getMostRecentCommit(octokitClient: Octokit, query: string): Promise<Commit | null> {
+  const commitSearchResults = await octokitClient.rest.search.commits({
+    q: `${query} author-date:<${(new Date()).toISOString()}`,
+    sort: 'author-date',
+    order: 'desc',
+    per_page: 1
+  })
 
-  let complete = false
-  for (let page = 1; !complete; page++) {
-    const commitSearchResults = await octokitClient.rest.search.commits({ q: fullQuery, page })
-    complete = !commitSearchResults.data.incomplete_results
-    commits.push(...commitSearchResults.data.items.map<Commit>((commitResult) => {
-      return {
-        url: commitResult.html_url,
-        sha: commitResult.sha,
-        date: commitResult.commit.author.date,
-        author: commitResult.author?.login,
-        message: commitResult.commit.message
-      }
-    }))
-  }  
-  return commits
+  if (commitSearchResults.data.items.length === 0) return null
+  return {
+    url: commitSearchResults.data.items[0].html_url,
+    sha: commitSearchResults.data.items[0].sha,
+    date: commitSearchResults.data.items[0].commit.author.date,
+    author: commitSearchResults.data.items[0].author?.login,
+    message: commitSearchResults.data.items[0].commit.message,
+  }
 }
 
 // TODO
 // Get and store all commits with "fuck" that came after the most recent one gotten
-//    get the time of the most recent one gotten
-//    get all commits that come after a certain time (the most recent one)
-//    store all commits that were gotten
-// Fetch the oldest stored commit and delete it
-//    fetch the oldest one
+//    get the most recent commit that matches query
+//    check if it's already present in the db
+//    if not present, add to db
+// Fetch the newest stored commit and delete it
+//    fetch the newest one
 //    tweet it
 //    delete it
 
 async function main () {
   const octokitClient = new Octokit({})
   // console.log(await getCommits(octokitClient, 'Q'))
-  console.log(await getCommitsAfterDateTime(octokitClient, 'Q', '2023-07-01T16:15:49.000+05:30'))
+  console.log(await getMostRecentCommit(octokitClient, 'fuck'))
 }
 
 main().catch(console.error)
